56e49e9ac8f97a980451813a055f47fd3997808b
Dawid <dawid@dawid.pl>
2020-06-05 13:23:41 +0200

    my version 01 (2019.02.12)

--- client/main.cpp
+++ client/main.cpp
@@ -1,93 +1,68 @@
-/*
-** client.c -- a stream socket client demo
-*/
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <netdb.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
-
+#include <netdb.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h> //to get close() function to work
 
-#define PORT "3490" // the port client will be connecting to 
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+int main()
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
+    struct addrinfo hints_c, *res_c;
+    int status_c;
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
+    memset(&hints_c, 0, sizeof hints_c);
+    hints_c.ai_family = AF_UNSPEC;
+    hints_c.ai_socktype = SOCK_STREAM;
 
-int main(int argc, char *argv[])
-{
-    int sockfd, numbytes;  
-    char buf[MAXDATASIZE];
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
-
-    if (argc != 2) {
-        fprintf(stderr,"usage: client hostname\n");
-        exit(1);
-    }
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue;
-        }
-
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("client: connect");
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
+    if ( (status_c = getaddrinfo("dawid-VirtualBox", "3490", &hints_c, &res_c)) != 0 )
+    {
+        printf("getaddrinfo error: %s\n", gai_strerror(status_c));
         return 2;
     }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-            s, sizeof s);
-    printf("client: connecting to %s\n", s);
+    int socketFD_c = socket(res_c->ai_family, res_c->ai_socktype, res_c->ai_protocol);
+    bind(socketFD_c, res_c->ai_addr, res_c->ai_addrlen);
 
-    freeaddrinfo(servinfo); // all done with this structure
+    connect(socketFD_c, res_c->ai_addr, res_c->ai_addrlen);
+//-------------------------------------------------
 
-    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-        perror("recv");
-        exit(1);
+    char buffsend[128];
+    char buffrecv[128];
+
+    while(true)
+    {
+    //RECEIVING
+        printf("\n...waiting for message from server...\n");
+        int bytes_rcvd = recv(socketFD_c, buffrecv, 128-1, 0);
+        if (bytes_rcvd <= 0)
+        {
+            printf("Either connection closed or error\n");
+            break;
+        }
+        buffrecv[bytes_rcvd] = '\0';
+        printf("[Message from server]: %s\n", buffrecv);
+
+    //SENDING
+        printf("Enter your message for server: ");
+        fgets(buffsend, 128-1, stdin);
+
+        int bytes_sent = send(socketFD_c, buffsend, strlen(buffsend), 0);
+        if (bytes_sent == 0)
+        {
+            printf("Failure sending message\n");
+            close(socketFD_c);
+            break;
+        }
     }
 
-    buf[numbytes] = '\0';
 
-    printf("client: received '%s'\n",buf);
-
-    close(sockfd);
+    close(socketFD_c); //to fully free FD
+    freeaddrinfo(res_c);
 
     return 0;
 }
+
